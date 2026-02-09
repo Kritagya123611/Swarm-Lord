9a847aba6bd46100d213784398ba085955c13452
Kritagya123611 <jhakritagya45@gmail.com>
2026-02-09 11:05:26 +0530

    Initial commit: Setup project structure for Control Plane and Kinetic Layer

---
+++ kinetic_layer/go.mod
@@ -0,0 +1,3 @@
+module swarm/kinetic
+
+go 1.25.1
---
+++ kinetic_layer/main.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	fmt.Println("this is the layer for doing the file system/disk related operations in kinetic")
+}
